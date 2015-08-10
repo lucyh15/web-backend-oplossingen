eca12d338ade6889eb437cd81fad722548aa8425
Lucy <LocalAdmin@MECH-B107-10.syntraab.mechelen>
2015-08-10 13:28:14 +0200

    opdracht comment toevoegen

---
+++ opdracht-comment/opdracht.php
@@ -0,0 +1,21 @@
+<?php $naam= "lucy hendriks";
+/* dit is mijn emailadres*/ $email = "lucyhendriks@gmail.com";
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>opdracht-comment</title>
+
+</head>
+
+<body>
+
+<p>Hallo ik heet <?= $naam ?></p>
+<p> mijn emailadress is <?php echo $email ?></p>
+</body>
+
+
+</html>
